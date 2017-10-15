90f2d8188ee1240929321ff792289f73c965102d
benweiser <bweiser86@gmail.com>
2017-10-15 12:39:32 -0700

    Fix error in Grunt build, use config in npm-scripts

--- build-scripts/grunt-build/Gruntfile.js
+++ build-scripts/grunt-build/Gruntfile.js
@@ -1,3 +1,5 @@
+const browserSync = require("browser-sync");
+
 module.exports = function (grunt) {
 
     // Project configuration.
@@ -14,31 +16,40 @@ module.exports = function (grunt) {
             }
         },
         watch: {
+            options: {
+                spawn: false
+            },
             css: {
                 files: ["../../src/scss/**"],
-                tasks: ["sass"],
+                tasks: ["sass", "browserSync-inject"],
             }
         },
-        browserSync: {
-            bsFiles: {
-                src: "../../src/scss/**"
-            },
-            options: {
-                watchTask: true,
-                injectChanges: true,
-                open: false,
-                // Replace proxy with the location on your local WP site
-                proxy: "wordcamp2017.loc",
-                // You can call this whatever you want
-                tunnel: "wordcamp2017"
-            },
-        }
     });
 
     grunt.loadNpmTasks("grunt-sass");
     grunt.loadNpmTasks("grunt-contrib-watch");
-    grunt.loadNpmTasks("grunt-browser-sync");
+    //grunt.loadNpmTasks("grunt-browser-sync");
+
+    // Init BrowserSync manually
+    grunt.registerTask("browserSync-init", function () {
+        const done = this.async();
+        browserSync({
+            watchTask: true,
+            //injectChanges: true,
+            open: false,
+            // Replace proxy with the location on your local WP site
+            proxy: "wordcamp2017.loc",
+            // You can call this whatever you want
+            tunnel: "wordcamp2017"
+        }, function (err, bs) {
+            done();
+        });
+    });
+
+    grunt.registerTask("browserSync-inject", function () {
+        browserSync.reload(["../../assets/css/style.css"]);
+    });
 
     // Default task(s).
-    grunt.registerTask("default", ["browserSync", "watch"]);
+    grunt.registerTask("default", ["browserSync-init", "watch"]);
 };
